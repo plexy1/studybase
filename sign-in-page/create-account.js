268de479a805ebd868ff703a15d89095fc4f53a9
Rishab <rishabyadav6132@gmail.com>
2025-01-31 17:03:33 -0500

    working on firebase db

--- sign-in-page/create-account.js
+++ sign-in-page/create-account.js
@@ -1,1 +1,32 @@
-//To link to firebase.
+//To link to firebase(for user registration)
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzB_tnvVNaVOhItN_3Hw5zv325LmL1zko",
+  authDomain: "studybase2311.firebaseapp.com",
+  projectId: "studybase2311",
+  storageBucket: "studybase2311.firebasestorage.app",
+  messagingSenderId: "594569312000",
+  appId: "1:594569312000:web:1340ec389ee98f5c84a5e5",
+  measurementId: "G-E8E297LTXP"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+
+//input fields
+const floatingUsername = document.getElementById("floatingUsername").value;
+const floatingEmail = document.getElementById("floatingEmail").value;
+const floatingUniversity = document.getElementById("floatingUniversity").value;
+const floatingPassword = document.getElementById("floatingPassword").value;
+const submit = document.getElementById("submit");
+
+submit.addEventListener('click', function(event){
+event.preventDefault();
+alert("Sent request")
+})
