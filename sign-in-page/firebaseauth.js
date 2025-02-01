22637e76790e7cf4c2ff4c760ad8228a460b4548
Rishab <rishabyadav6132@gmail.com>
2025-02-01 18:40:34 -0500

    working on firebase signup

---
+++ sign-in-page/firebaseauth.js
@@ -0,0 +1,71 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { getFirestore,setDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxZAi4bKJxNSO7paLae8rUDz6DxYbFc7o",
+  authDomain: "studybase-data.firebaseapp.com",
+  projectId: "studybase-data",
+  storageBucket: "studybase-data.firebasestorage.app",
+  messagingSenderId: "471482464641",
+  appId: "1:471482464641:web:46fe6cf41e17a24e785080"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+function showMessage(message, divId){
+    var messageDiv=document.getElementById(divId);
+    messageDiv.innerHTML=message;
+
+
+
+}
+const signUp=document.getElementById('submit-register');
+signUp.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const username=document.getElementById('floatingUsername').value;
+    const email=document.getElementById('floatingEmail').value;
+    const university=document.getElementById('floatingUniversity').value;
+    const password=document.getElementById('floatingPassword').value;
+
+
+    const auth = getAuth();
+    const db = getFirestore();
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential)=>{
+        const user=userCredential.user;
+        const userData={
+            email: email,
+            university: university,
+
+        };
+        showMessage('Account created successfully','signIpMessage' );
+        const docRef=doc(db, "users", user.uid);
+        setDoc(docRef, userData)
+        .then(()=>{ 
+            window.location.href="index.html";
+        })
+        .catch((error)=>{
+            console.error("error writing document", error);
+        })
+    })
+
+
+    .catch((error)=>{
+        const errorCode=error.errorCode;
+        if(errorCode=='auth/email-already-in-use'){
+            showMessage('Email already registered to a studybase account', 'signUpMessage');
+
+        }
+        else{
+            showMessage('Unable to create user', 'signUpMessage');
+        }
+    })
+    
+
+
+})
